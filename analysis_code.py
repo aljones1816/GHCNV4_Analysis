86ec6bb3c1107a814a96e0d7a860517d1b276d95
aljones1816 <aljones1816@gmail.com>
2021-04-21 17:53:19 -0400

    cleaned up code formatting

--- analysis_code.py
+++ analysis_code.py
@@ -32,7 +32,7 @@ stnMeta = pd.read_fwf(GHCNmeta, colspecs=[(0, 2), (0, 12), (12, 21), (21, 31),
                       names=['country_code', 'station',
                              'lat', 'lon', 'elev', 'name'])
 
-#create grids
+# create grids
 grid_size = 5
 count = -90 + (grid_size / 2)
 stnMeta['latgrid'] = 0
@@ -50,45 +50,50 @@ for x in range(-180, 180, 5):
 
 stnMeta['gridbox'] = stnMeta['latgrid'].map(str) + " lat " + stnMeta['longrid'].map(str) + " lon"
 stnMetaGrid = stnMeta.merge(lndmsk, on='gridbox')
-stnMetaGrid['grid_weight'] = nm.sin((stnMetaGrid['latgrid']+grid_size/2)*nm.pi/180) - nm.sin((stnMetaGrid['latgrid']-grid_size/2)*nm.pi/180)
-stnMetaGrid['grid_weight'] = stnMetaGrid['grid_weight']*stnMetaGrid['land_percent']
+stnMetaGrid['grid_weight'] = nm.sin((stnMetaGrid['latgrid'] + grid_size / 2) * nm.pi / 180) - nm.sin(
+    (stnMetaGrid['latgrid'] - grid_size / 2) * nm.pi / 180)
+stnMetaGrid['grid_weight'] = stnMetaGrid['grid_weight'] * stnMetaGrid['land_percent']
 
 # clean ghcn and create anomalies
 ghcnv4NoNullYears = ghcnv4[ghcnv4.year.notnull()]
-ghcnv4NoNullYears = ghcnv4NoNullYears.replace(-9999,None)
-for m in range(1,13):
-    ghcnv4NoNullYears['VALUE' + str(m)] = ghcnv4NoNullYears['VALUE' + str(m)]/100
+ghcnv4NoNullYears = ghcnv4NoNullYears.replace(-9999, None)
+for m in range(1, 13):
+    ghcnv4NoNullYears['VALUE' + str(m)] = ghcnv4NoNullYears['VALUE' + str(m)] / 100
 
 ghcnlong = ghcnv4NoNullYears.set_index('station')
 ghcnlong = ghcnlong.reset_index()
-ghcnlong = pd.melt(ghcnlong,id_vars=['station','year'], value_vars=['VALUE1','VALUE2','VALUE3','VALUE4','VALUE5','VALUE6',
-                                                'VALUE7','VALUE8','VALUE9','VALUE10','VALUE11','VALUE12'])
+ghcnlong = pd.melt(ghcnlong, id_vars=['station', 'year'],
+                   value_vars=['VALUE1', 'VALUE2', 'VALUE3', 'VALUE4', 'VALUE5', 'VALUE6',
+                               'VALUE7', 'VALUE8', 'VALUE9', 'VALUE10', 'VALUE11', 'VALUE12'])
 
-ghcnBaselines = ghcnlong[ghcnlong['year'].between(1961,1990)]
-ghcnBaselines = ghcnBaselines.drop(columns = 'year')
-ghcnBaselines = ghcnBaselines.groupby(['station','variable']).mean()
-ghcnBaselines = ghcnBaselines.rename(columns={"value" : "baseline"})
+ghcnBaselines = ghcnlong[ghcnlong['year'].between(1961, 1990)]
+ghcnBaselines = ghcnBaselines.drop(columns='year')
+ghcnBaselines = ghcnBaselines.groupby(['station', 'variable']).mean()
+ghcnBaselines = ghcnBaselines.rename(columns={"value": "baseline"})
 
-ghcnAnoms = ghcnlong.merge(ghcnBaselines, on=['station','variable'])
+ghcnAnoms = ghcnlong.merge(ghcnBaselines, on=['station', 'variable'])
 ghcnAnoms['anomalies'] = ghcnAnoms['value'] - ghcnAnoms['baseline']
 
-#merge on the metadata
+# merge on the metadata
 ghcnAnomsGrid = ghcnAnoms.merge(stnMetaGrid, on=['station'])
 ghcnAnomsGrid = ghcnAnomsGrid[ghcnAnomsGrid.anomalies.notnull()]
-ghcnAnomsGrid = ghcnAnomsGrid.drop(columns = ['baseline','lat','lon','elev','latgrid','longrid','land_percent',
-                              'ocean_percent','value'])
-ghcnAnomsGrid = ghcnAnomsGrid.groupby(['gridbox','variable','year']).mean().reset_index()
+ghcnAnomsGrid = ghcnAnomsGrid.drop(columns=['baseline', 'lat', 'lon', 'elev', 'latgrid', 'longrid', 'land_percent',
+                                            'ocean_percent', 'value'])
+ghcnAnomsGrid = ghcnAnomsGrid.groupby(['gridbox', 'variable', 'year']).mean().reset_index()
 print(ghcnAnomsGrid.columns.tolist())
 
+
 def weighted_average(group):
-   weights = group['grid_weight']
-   anomalies = group['anomalies']
-   return nm.average(anomalies,weights=weights)
-ghcnAnomsWtd = ghcnAnomsGrid.groupby(['variable','year']).apply(func = weighted_average).reset_index()
+    weights = group['grid_weight']
+    anomalies = group['anomalies']
+    return nm.average(anomalies, weights=weights)
+
+
+ghcnAnomsWtd = ghcnAnomsGrid.groupby(['variable', 'year']).apply(func=weighted_average).reset_index()
 ghcnAnomsWtd.columns.values[2] = "anomalies"
 
 simpleTemp = ghcnAnomsWtd.groupby('year').mean('anomalies').reset_index()
-simpleTemp = simpleTemp[simpleTemp['year'].between(1900,2020)]
+simpleTemp = simpleTemp[simpleTemp['year'].between(1900, 2020)]
 
 # plot temperature record
 plt.plot(simpleTemp['year'], simpleTemp['anomalies'])
