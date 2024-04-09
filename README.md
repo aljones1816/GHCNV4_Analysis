aedd13474c8f23e8ba62865241843d59cb70bd42
Alan <aljones1816@gmail.com>
2024-04-09 13:41:27 -0400

    update project to run locally

--- README.md
+++ README.md
@@ -1,23 +1,40 @@
 # GHCNV4_Analysis
 
-This simple program will take the GHCN V4 QCU (quality controlled unadjusted, or raw) monthly station data and generate a global, land-only mean temperature anomaly series using gridded anomalies.
+This simple program will take the GHCN V4 QCU (quality controlled unadjusted, or raw) monthly station data and generate a global, land-only mean temperature anomaly series using gridded anomalies. The results are output to a csv file for further analysis.
 
 A comparison between SimpleTemp and other global temperature datasets from organizations like NASA and the UK's Met Office can be seen [here](https://i.imgur.com/TbtHeLB.png).
 
 ## Setup
-Analysis uses the GHCNV4 monthly unadjusted station data. The script should now automatically download the dataset needed, but users can also access it [here](https://www.ncdc.noaa.gov/data-access/land-based-station-data/land-based-datasets/global-historical-climatology-network-monthly-version-4).
 
-Users will also need a land mask file, the one I used is [here](https://drive.google.com/file/d/1nSDlTfMbyquCQflAvScLM6K4dvgQ7JBj/view?usp=sharing).
+clone the repository
+
+```bash
+git clone https://github.com/aljones1816/GHCNV4_Analysis.git
+
+cd GHCNV4_Analysis
+```
+
+Set up and activate a virtual environment
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+```
+
+Install the required dependencies
+
+```bash
+pip3 install -r requirements.txt
+```
 
 ## To run the analysis
 
-1. Download the required datasets from GHCN/Dropbox
-2. Run ```pip3 install -r requirements.txt``` to install necessary dependencies
-4. Run the analysis.py script
-
-It is recommended that you run this code in a virtual environment to keep dependency management straightforward.
+```bash
+python3 main.py
+```
 
 ## Overview
+
 This dataset is a global land average. The method I used is extremely simple (so I've called the dataset simpleTemp), and applies no adjustments or corrections to the raw data whatsoever. The basic outline is as follows:
 
 - The stations are averaged into 5x5 grids. This is because some areas of the globe have a higher station density than others, so those areas will be unequally weighted if the stations are simply averaged individually.
@@ -29,4 +46,3 @@ Importantly, I have omitted any additional processing steps (adjustments), inclu
 - There is no correction made for time of observation bias. This produces a very small bias for the whole globe, but is significant for regional changes, particularly in the contiguous US, where the station network is particularly dense and transient.
 - There is no homogenization applied. This means that urbanization bias and station moves are not accounted for. This, again, only produces a slight bias for the global mean trend, but can be significant at the regional level.
 - There is no spatial interpolation for grid boxes that have no stations - these cells are simply absent from the dataset. As a result, the polar regions are under-sampled, and because the Arctic is one of the fastest warming regions on Earth, this produces a negative bias in the global trend. This approach is similar to that used by HadCRUT in versions below 5, and consequently my analysis is most similar to HadCRUT.
-
--- analysis_code.py
+++ analysis_code.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import numpy as nm
+import numpy as np
 import urllib.request
 import tarfile
 import glob
-from google_drive_downloader import GoogleDriveDownloader as gdd
+import gdown
 import shutil
 
 dirList = glob.glob('ghcnm*')
@@ -20,9 +20,12 @@ ftpstream = urllib.request.urlopen(ghcnurl)
 ghcnurl = tarfile.open(fileobj=ftpstream, mode="r|gz")
 ghcnurl.extractall()
 
-gdd.download_file_from_google_drive(file_id='1nSDlTfMbyquCQflAvScLM6K4dvgQ7JBj',
-                                    dest_path='./landmask.dta',
-                                    unzip=False)
+# only download landmask if file not already present
+if not glob.glob('landmask.dta'):
+    landmask_url = 'https://drive.google.com/uc?id=1nSDlTfMbyquCQflAvScLM6K4dvgQ7JBj'
+    landmask_output = './landmask.dta'
+    gdown.download(landmask_url, landmask_output, quiet=False)
+
 
 name = glob.glob('ghcnm*')
 
@@ -50,23 +53,24 @@ ghcnv4 = pd.read_fwf(GHCNDat[0],
 # load landmask
 lndmsk = pd.read_stata(landmask)
 
+
 # Load station metadata
 stnMeta = pd.read_fwf(GHCNmeta[0], colspecs=[(0, 2), (0, 12), (12, 21), (21, 31),
                                           (31, 38), (38, 69)],
                       names=['country_code', 'station',
                              'lat', 'lon', 'elev', 'name'])
-
+ 
 # create grids
 grid_size = 5
 count = -90 + (grid_size / 2)
-stnMeta['latgrid'] = 0
+stnMeta['latgrid'] = 0.0
 
 for x in range(-90, 90, 5):
     stnMeta.loc[stnMeta['lat'].between(x, x+5), 'latgrid'] = count
     count = count + grid_size
 
 count = -180 + (grid_size / 2)
-stnMeta['longrid'] = 0
+stnMeta['longrid'] = 0.0
 
 for x in range(-180, 180, 5):
     stnMeta.loc[stnMeta['lon'].between(x, x+5), 'longrid'] = count
@@ -74,25 +78,16 @@ for x in range(-180, 180, 5):
 
 stnMeta['gridbox'] = stnMeta['latgrid'].map(str) + " lat " + stnMeta['longrid'].map(str) + " lon"
 stnMetaGrid = stnMeta.merge(lndmsk, on='gridbox')
-stnMetaGrid['grid_weight'] = nm.sin((stnMetaGrid['latgrid'] + grid_size / 2) * nm.pi / 180) - nm.sin(
-    (stnMetaGrid['latgrid'] - grid_size / 2) * nm.pi / 180)
+stnMetaGrid['grid_weight'] = np.sin((stnMetaGrid['latgrid'] + grid_size / 2) * np.pi / 180) - np.sin(
+    (stnMetaGrid['latgrid'] - grid_size / 2) * np.pi / 180)
 stnMetaGrid['grid_weight'] = stnMetaGrid['grid_weight'] * stnMetaGrid['land_percent']
 
 # clean ghcn and create anomalies
-
-
-
-ghcnv4NoNullYears =  ghcnv4.replace(-9999, nm.nan)
-
-test0 = ghcnv4[ghcnv4['year'].between(2020, 2022)]
-test1 = ghcnv4NoNullYears[ghcnv4NoNullYears['year'].between(2020, 2022)]
+ghcnv4NoNullYears =  ghcnv4.replace(-9999, np.nan)
 
 for m in range(1, 13):
     ghcnv4NoNullYears['VALUE' + str(m)] = ghcnv4NoNullYears['VALUE' + str(m)] / 100
 ghcnv4NoNullYears = ghcnv4NoNullYears[ghcnv4NoNullYears.year.notnull()]
-
-
-test2 = ghcnv4NoNullYears[ghcnv4NoNullYears['year'].between(2021, 2022)]
 
 ghcnlong = ghcnv4NoNullYears.set_index('station')
 ghcnlong = ghcnlong.reset_index()
@@ -111,20 +106,22 @@ ghcnAnoms['anomalies'] = ghcnAnoms['value'] - ghcnAnoms['baseline']
 # merge on the metadata
 ghcnAnomsGrid = ghcnAnoms.merge(stnMetaGrid, on=['station'])
 ghcnAnomsGrid = ghcnAnomsGrid[ghcnAnomsGrid.anomalies.notnull()]
-ghcnAnomsGrid = ghcnAnomsGrid.drop(columns=['baseline', 'lat', 'lon', 'elev', 'latgrid', 'longrid', 'land_percent',
-                                            'ocean_percent', 'value'])
+ghcnAnomsGrid = ghcnAnomsGrid.drop(columns=['baseline', 'station', 'name', 'lat', 'lon', 'elev', 'latgrid', 'longrid', 'land_percent',
+                                            'ocean_percent', 'value', 'country_code'])
+
+# take the mean of the anomalies grouped by gridbox, variable, and year
 ghcnAnomsGrid = ghcnAnomsGrid.groupby(['gridbox', 'variable', 'year']).mean().reset_index()
 
+# take weighted average of anomalies using the grid_weight column as weights
+ghcnAnomsWtd = ghcnAnomsGrid.groupby(['year']).apply(lambda x: np.average(x['anomalies'], weights=x['grid_weight'])).reset_index()
 
+# rename the columns
+ghcnAnomsWtd.columns = ['year', 'anomalies']
 
-def weighted_average(group):
-    weights = group['grid_weight']
-    anomalies = group['anomalies']
-    return nm.average(anomalies, weights=weights)
+# filter to only years between 1900 and present
+ghcnAnomsWtd = ghcnAnomsWtd[ghcnAnomsWtd.year >= 1900]
 
-
-ghcnAnomsWtd = ghcnAnomsGrid.groupby(['variable', 'year']).apply(func=weighted_average).reset_index()
-ghcnAnomsWtd.columns.values[2] = "anomalies"
-
+# save the data to a csv file
+ghcnAnomsWtd.to_csv('data/raw_ghcnm_anomalies.csv', index=False)
 
 
